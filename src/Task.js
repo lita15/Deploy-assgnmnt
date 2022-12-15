0ae02fcaae5ca8d539898437330c6d436853f4b4
Noor Yulita Apsari <nooryulitaapsari@students.unnes.ac.id>
2022-12-15 17:20:43 +0700

    assignment push

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Noor Yulita Apsari"; // TODO: replace this
+const studentId = "YFE2606679"; // TODO: replace this
+
+export { url, studentName, studentId };
